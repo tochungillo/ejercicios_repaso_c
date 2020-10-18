abc67d800a711a899426e00b0b63b7cad6369a55
tochungillo <andy98lopez@gmail.com>
2020-10-18 18:47:30 +0200

    =all exercices from c#

---
+++ DateTimeChallenge/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DateTimeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "", b = "", c = "", d = "";
+            Boolean correcto = false;
+            while (correcto == false)
+            {
+                Console.WriteLine("what date format you want to use? (dd/MM/yy || MM/dd/yy)");
+                a = Console.ReadLine();
+                if (a.Equals("dd/MM/yy"))
+                    correcto = true;
+                if (a.Equals("MM/dd/yy"))
+                    correcto = true;
+            }
+            correcto = false;
+            while (correcto == false)
+            {
+                Console.WriteLine("Give me a date");
+                b = Console.ReadLine();
+            }
+            
+            correcto = false;
+            while (correcto == false)
+            {
+                Console.WriteLine("what time format you want to use? (24 || 12)");
+                c = Console.ReadLine();
+            }
+            
+            correcto = false;
+            while (correcto == false)
+            {
+                Console.WriteLine("Give me a time");
+                d = Console.ReadLine();
+            }
+
+            Console.WriteLine(a+" "+b+" "+c+" "+d);
+        }
+    }
+}
---
+++ TextFileChallange/User.cs
@@ -0,0 +1,26 @@
+﻿namespace TextFileChallange
+{
+    internal class User
+    {
+        private string firstName, lastName;
+        private int age;
+        private string alive;
+
+        public User(string firstName, string lastName, int age, bool alive)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            if (alive)
+                this.alive = "Alive";
+            else
+                this.alive = "Dead";
+        }
+
+        public string getFirstName() { return firstName; }
+        public string getLastName() { return lastName; }
+        public int getAge() { return age; }
+        public string getAlive() { return alive; }
+
+    }
+}
